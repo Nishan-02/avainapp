d4ab89d6e3f44d79adb438e581cb835b2066149a
Nishan-02 <nishansuvarna761@gmail.com>
2026-02-13 21:38:56 +0530

    update API URL

--- frontend/src/components/Predictor.js
+++ frontend/src/components/Predictor.js
@@ -1,7 +1,7 @@
 // src/components/Predictor.js
 import React, { useState, useEffect } from 'react';
 
-const BASE_API_URL = process.env.REACT_APP_API_BASE_URL || 'https://avainapp.onrender.com';
+const BASE_API_URL = process.env.REACT_APP_API_BASE_URL || 'https://avainapp.onrender.com/predict';
 
 function Predictor() {
   const [models, setModels] = useState([]);
